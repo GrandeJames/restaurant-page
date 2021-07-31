54072d020622cf147e9edaae867a2fab1438716d
James Grande <james.grande6@gmail.com>
2021-07-30 18:02:43 -1000

    Fix project structure

---
+++ src/modules/header.js
@@ -0,0 +1,22 @@
+export function addHeader(parentElement) {
+  const header = document.createElement("header");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = "FOODIES";
+
+  const nav = document.createElement("nav");
+
+  const a1 = document.createElement("a");
+  const a3 = document.createElement("a");
+  const a2 = document.createElement("a");
+
+  a1.textContent = "HOME";
+  a2.textContent = "MENU";
+  a3.textContent = "CONTACT";
+
+  nav.appendChild(a1);
+  nav.appendChild(a2);
+  nav.appendChild(a3);
+
+  parent.appendChild(header);
+}
