d0fb13b6711004e352bae3a99ddb5a14633714ec
James Grande <james.grande6@gmail.com>
2021-07-30 20:38:02 -1000

    Add homepage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import { addHeader } from "./modules/header.js";
+import { addHome } from "./modules/home.js";
 import "./styles.css";
 
 function init() {
   const content = document.querySelector("#content");
   addHeader(content);
+
+  const main = document.createElement("main");
+  addHome(main);
+  content.appendChild(main);
 }
 
 init();
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,1 +1,24 @@
-import { addHeader } from "./modules/header.js";
+import background from "../images/home-background.jpg";
+
+export function addHome(parentElement) {
+  const topSection = document.createElement("div");
+  const bottomSection = document.createElement("div");
+
+  topSection.id = "top-section";
+  bottomSection.id = "bottom-section";
+
+  const h2 = document.createElement("h2");
+  h2.innerHTML = `Eat.<br>Drink.<br>Love.`;
+  topSection.appendChild(h2);
+
+  const button = document.createElement("button");
+  button.textContent = "View menu";
+  topSection.appendChild(button);
+
+  const image = document.createElement("img");
+  image.src = background;
+  bottomSection.appendChild(image);
+
+  parentElement.appendChild(topSection);
+  parentElement.appendChild(bottomSection);
+}
